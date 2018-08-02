3673473f2e84181d4940a7106f28e3d2a7c9a118
Omelchenko Maksym <omelchenko.maksym@pdffiller.team>
2018-08-02 16:51:36 +0300

    Add migration check

--- src/migrations/2013_04_09_062329_create_revisions_table.php
+++ src/migrations/2013_04_09_062329_create_revisions_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
 
 class CreateRevisionsTable extends Migration
 {
@@ -11,18 +12,20 @@ class CreateRevisionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('revisions', function ($table) {
-            $table->increments('id');
-            $table->string('revisionable_type');
-            $table->integer('revisionable_id');
-            $table->integer('user_id')->nullable();
-            $table->string('key');
-            $table->text('old_value')->nullable();
-            $table->text('new_value')->nullable();
-            $table->timestamps();
+        if (!Schema::hasTable('revisions')) {
+            Schema::create('revisions', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('revisionable_type');
+                $table->integer('revisionable_id');
+                $table->integer('user_id')->nullable();
+                $table->string('key');
+                $table->text('old_value')->nullable();
+                $table->text('new_value')->nullable();
+                $table->timestamps();
 
-            $table->index(array('revisionable_id', 'revisionable_type'));
-        });
+                $table->index(array('revisionable_id', 'revisionable_type'));
+            });
+        }
     }
 
     /**
@@ -32,6 +35,6 @@ class CreateRevisionsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('revisions');
+        Schema::dropIfExists('revisions');
     }
 }
